26aeaafe97666acee8e67939d2355f3ba076facc
Poveii <yellowtutors@gmail.com>
2023-06-30 19:34:42 -0300

    feat: Implements first route

--- src/server.js
+++ src/server.js
@@ -1,9 +1,20 @@
 import http from "node:http"
 
 import { json } from "./middlewares/json.js"
+import { routes } from "./routes.js"
 
 const server = http.createServer(async (req, res) => {
+  const { method, url } = req
+
   await json(req, res)
+
+  const actualRoute = routes.find((route) => {
+    return route.method === method
+  })
+
+  if (actualRoute) {
+    return actualRoute.handler(req, res)
+  }
 
   return res.writeHead(404).end()
 })
