a9bf3dd7020291521ed8976807d346aa968de5a7
Poveii <yellowtutors@gmail.com>
2023-07-14 19:25:15 -0300

    feat: Local database persistence

--- src/database.js
+++ src/database.js
@@ -1,1 +1,21 @@
-export class Database { }
+import fs from 'node:fs/promises'
+
+const databasePath = new URL("../db.json", import.meta.url)
+
+export class Database {
+  #database = {}
+
+  constructor() {
+    fs.readFile(databasePath, "utf-8")
+      .then((data) => {
+        this.#database = JSON.parse(data)
+      })
+      .catch(() => {
+        this.#persists()
+      })
+  }
+
+  #persists() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
+  }
+}
