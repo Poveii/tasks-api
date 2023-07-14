69a571f8b91c7e12584a94c0bdb71ac55f652027
Poveii <yellowtutors@gmail.com>
2023-07-14 19:53:44 -0300

    style: All codes formatted by Prettier

--- src/database.js
+++ src/database.js
@@ -1,39 +1,39 @@
-import fs from 'node:fs/promises'
+import fs from "node:fs/promises";
 
-const databasePath = new URL("../db.json", import.meta.url)
+const databasePath = new URL("../db.json", import.meta.url);
 
 export class Database {
-  #database = {}
+  #database = {};
 
   constructor() {
     fs.readFile(databasePath, "utf-8")
       .then((data) => {
-        this.#database = JSON.parse(data)
+        this.#database = JSON.parse(data);
       })
       .catch(() => {
-        this.#persists()
-      })
+        this.#persists();
+      });
   }
 
   #persists() {
-    fs.writeFile(databasePath, JSON.stringify(this.#database))
+    fs.writeFile(databasePath, JSON.stringify(this.#database));
   }
 
   insert(table, data) {
     if (Array.isArray(this.#database[table])) {
-      this.#database[table].push(data)
+      this.#database[table].push(data);
     } else {
-      this.#database[table] = [data]
+      this.#database[table] = [data];
     }
 
-    this.#persists()
+    this.#persists();
 
-    return data
+    return data;
   }
 
   select(table) {
-    let data = this.#database[table] ?? []
+    let data = this.#database[table] ?? [];
 
-    return data
+    return data;
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
-import { buildRoutePath } from "./utils/build-route-path.js"
+import { buildRoutePath } from "./utils/build-route-path.js";
 
 export const routes = [
   {
     method: "GET",
     path: buildRoutePath("/tasks"),
     handler: (req, res) => {
-      return res.end(JSON.stringify([]))
-    }
-  }
-]
+      return res.end(JSON.stringify([]));
+    },
+  },
+];
