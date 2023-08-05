8f0ddf4280211c5e5a4df8c98dd1124be43177c1
Poveii <yellowtutors@gmail.com>
2023-08-04 22:21:06 -0300

    feat: POST method to create a task

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,9 @@
+import { randomUUID } from 'node:crypto'
+
 import { buildRoutePath } from './utils/build-route-path.js'
+import { Database } from './database.js'
+
+const database = new Database()
 
 export const routes = [
   {
@@ -12,7 +17,20 @@ export const routes = [
     method: 'POST',
     path: buildRoutePath('/tasks'),
     handler: (req, res) => {
-      return res.end(JSON.stringify([]))
+      const { title, description } = req.body
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        created_at: new Date(),
+        updated_at: null,
+        completed_at: null,
+      }
+
+      database.insert('tasks', task)
+
+      return res.writeHead(201).end()
     },
   },
 ]
