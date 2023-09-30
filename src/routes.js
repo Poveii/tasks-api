b72a0c36248e401e44c0b59faeb6c76479e5ac05
Poveii <yellowtutors@gmail.com>
2023-09-29 21:25:14 -0300

    feat: GET and POST validations
    + Implements search send for select method in GET route

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,17 @@ export const routes = [
     method: 'GET',
     path: buildRoutePath('/tasks'),
     handler: (req, res) => {
-      const tasks = database.select('tasks')
+      const { title, description } = req.query
+
+      let search = null
+      if (title !== undefined || description !== undefined) {
+        search = {
+          title,
+          description,
+        }
+      }
+
+      const tasks = database.select('tasks', search)
 
       return res.end(JSON.stringify(tasks))
     },
@@ -20,6 +30,18 @@ export const routes = [
     path: buildRoutePath('/tasks'),
     handler: (req, res) => {
       const { title, description } = req.body
+
+      if (!title) {
+        return res
+          .writeHead(404)
+          .end(JSON.stringify({ message: 'title is required' }))
+      }
+
+      if (!description) {
+        return res
+          .writeHead(404)
+          .end(JSON.stringify({ message: 'description is required' }))
+      }
 
       const task = {
         id: randomUUID(),
